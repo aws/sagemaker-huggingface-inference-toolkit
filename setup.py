44e3decd8aab4a710ef5f1094c39818cf7ea0f28
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2023-08-15 13:30:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools import find_packages, setup
 # We don't declare our dependency on transformers here because we build with
 # different packages for different variants
 
-VERSION = "2.2.0"
+VERSION = "2.3.0.dev0"
 
 
 # Ubuntu packages
